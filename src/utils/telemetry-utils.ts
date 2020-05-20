844f28e771701657db9f945734e5740679efb4aa
Andrew Turner <ajturner@highearthorbit.com>
2020-05-20 13:57:00 -0400

    Updated telemetry

--- src/utils/telemetry-utils.ts
+++ src/utils/telemetry-utils.ts
@@ -1,13 +1,15 @@
+
 // Sends an event via the document for the <hub-telemetry> component
 export const sendTelemetry = ({category, action, label}):boolean => {
     console.log("sendTelemetry", {category, action, label})
-    var event = new CustomEvent("hub-telemetry-event", {
-        detail: {
-            category: category, 
-            action: action, 
-            label: label
-        }
-    });
+    
+    const detail = {
+        category: category, 
+        action: action, 
+        label: label
+    };
+    // state.telemetry.logEvent(detail)
 
-  return window.dispatchEvent(event);
+    var event = new CustomEvent("hub-telemetry-event", { detail });
+    return window.dispatchEvent(event);
 }
