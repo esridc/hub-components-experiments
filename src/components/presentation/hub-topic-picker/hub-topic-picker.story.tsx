458154ffb254ed8fb7e6eebb607e8b7726b5b93f
Andrew Turner <ajturner@highearthorbit.com>
2020-07-06 14:45:44 -0400

    Fixed topic picker

--- src/components/presentation/hub-topic-picker/hub-topic-picker.story.tsx
+++ src/components/presentation/hub-topic-picker/hub-topic-picker.story.tsx
@@ -3,10 +3,10 @@ import readme from './readme.md'
 /* radar-map.story.js */
 export default function(stories, knobs) {
     const mainEl = document.createElement('div'); // do this OUTSIDE the render function below
-    const allowSelection = knobs.boolean('allow-selection', true);
-
 
     stories.add('Hub Topic Picker', () => {
+        const allowSelection = knobs.boolean('allow-selection', true);
+
         mainEl.innerHTML = `<hub-topic-picker
             allow-selection=${allowSelection}
         ></hub-topic-picker>`
